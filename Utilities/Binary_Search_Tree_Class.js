2a7a9c532e1f96c0cef2556dd6546073b4cbe58a
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-09-10 23:38:25 -0400

    Create Binary_Search_Tree_Class.js

---
+++ Utilities/Binary_Search_Tree_Class.js
@@ -0,0 +1,56 @@
+//A Binary Search Tree (BST)
+// Binary Search Tree utility class
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  }
+
+  // Insert specific value to the tree
+  insert(val) {
+    const node = new Node(val);
+    if (!this.head){
+      this.head = node;
+      this.head.left = null;
+      this.head.right = null;
+    }
+    else {
+      let p = this.head;
+      if (val < p.val){
+        while (val < p.val && p.left)
+          p = p.left;
+        node.left = p.left;
+        p.left = node;
+      }
+      else if (val > p.val){
+        while (val > p.val && p.right)
+          p = p.right;
+        node.right = p.right;
+        p.right = node;
+      }
+    }
+  }
+
+  // Remove specific value from the tree
+  remove(val) {
+  }
+}
+
+const bst = new BinarySearchTree();
+console.log(bst);
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(12);
+bst.insert(7);
+bst.insert(6);
+console.log(bst);
+console.log(bst.remove(7));
