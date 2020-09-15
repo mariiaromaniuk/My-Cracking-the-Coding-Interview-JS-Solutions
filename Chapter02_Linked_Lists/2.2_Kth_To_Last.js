7bef4586e1c3be5936dc8c114bf4a4a4b88c1167
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-09-14 22:14:26 -0400

    Update 2.2_Kth_To_Last.js

--- Chapter02_Linked_Lists/2.2_Kth_To_Last.js
+++ Chapter02_Linked_Lists/2.2_Kth_To_Last.js
@@ -3,20 +3,28 @@
 const LinkedList = require("../Utilities/Linked_List_Class.js");
 
 // Time: O(n), Space: O(1)
-function KthToLast1(list, k) {
+function kthToLast(list, k){
   if (!list) 
     throw Error('Invalid list');
 
   let fastP = list, slowP = list;
 
-  for (let i = 0; i < k; i++) {
+  for (let i = 0; i < k; i++){
     if (!fastP.next) 
       throw Error('list is not long enough');
     fastP = fastP.next;
   }
-  while (fastP.next) {
+  while (fastP.next){
     fastP = fastP.next;
     slowP = slowP.next;
   }
   return slowP.val;
 }
+
+// Test
+let list = new LinkedList();
+for (let elem of [1, 1, 5, 1, 10, 6, 8, 6, 9, 9, 10, 8]) 
+  list.addToTail(elem);
+
+kthToLast(list, 5);
+console.log(list.toArray());
